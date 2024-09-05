1709e455286d5568af824c92c310474d26cf2466
Daniel Jaušovec <jausovec.daniel@gmail.com>
2024-09-05 18:34:47 +0200

    Fix builder

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/olaf-sample-app"));
+app.use(express.static(__dirname + "/dist/olaf-sample-spa"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/olaf-sample-app/browser/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/olaf-sample-spa/index.html"));
 });
 app.listen(process.env.PORT || 8080);
